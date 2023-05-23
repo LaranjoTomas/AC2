70f3e94c383a3c583070320bd3b3b2fbf220df68
Laranjo <tomas.laranjo@ua.pt>
2023-05-23 10:55:28 +0100

    exerAula8

--- Aula8/Parte1/Exer2.c
+++ Aula8/Parte1/Exer2.c
@@ -1,7 +1,7 @@
 #include <detpic32.h>
 int main(void) {
     T3CONbits.TCKPS = 7; // 1:32 prescaler (i.e. fout_presc = 625 KHz)
-    PR3 = 62499; // Fout = 20MHz / (32 * (62499 + 1)) = 2 Hz
+    PR3 = 39061; // Fout = 20MHz / (32 * (62499 + 1)) = 2 Hz
     TMR3 = 0; // Clear timer T3 count register
     T3CONbits.TON = 1; // Enable timer T3 (must be the last command of the
     // timer configuration sequence)
